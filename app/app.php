44ced0b304dc2d86c045182334f22dc86b0edd70
Jose <jtorres.pdx@gmail.com>
2015-08-07 16:26:33 -0700

    This commit adds all final changes before code review. Contacts still not printing to '/' page.

--- app/app.php
+++ app/app.php
@@ -5,21 +5,23 @@
     /* Perhaps add contacts here? Unclear on instructions whether they should be a few already save here. */
 
     if (empty($_SESSION['list_of_contacts'])) {
-        $_SESSION['list_of_contacts'] = array();
+        $_SESSION['list_of_contacts'] = array('');
     }
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
-
+    
     $app->get("/", function() use ($app) {
+
         return $app['twig']->render('contacts.html.twig', array('list_of_contacts' => Contacts::getAll()));
     });
 
     $app->post("/create_contact", function() use ($app) {
         $contact = new Contacts($_POST['name'], $_POST['phone_number'], $_POST['address']);
         $contact->save();
+
         return $app['twig']->render('create_contact.html.twig', array('newcontact' => $contact));
     });
 
